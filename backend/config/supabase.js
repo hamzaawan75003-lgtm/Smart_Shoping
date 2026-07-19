e51db39a02b5a8c3b1393c7b82ebeae5b9808149
hamzaawan75003-lgtm <hamzaawan75003@gmail.com>
2026-07-19 11:33:07 +0500

    Update supabase.js

--- backend/config/supabase.js
+++ backend/config/supabase.js
@@ -2,7 +2,6 @@ const { createClient } = require('@supabase/supabase-js')
 
 const supabase = createClient(
   'https://wkhcgqbtzkwhuuurhujc.supabase.co',
-  '.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndraGNncWJ0emt3aHV1dXJodWpjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3Njg0Mzk0MSwiZXhwIjoyMDkyNDE5OTQxfQ.18Ni0Kp5pjayeXcTmiC34q3ohjOEqbvxsFOt9x7U0b0'
-)
+  '
 
 module.exports = supabase
